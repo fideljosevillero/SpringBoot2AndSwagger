086a3065fb7222cea21beaaf56bc0d9163328583
Fidel Jose <fideljose1708@hotmail.com>
2019-11-04 00:12:43 -0500

    Add custome metadata

--- src/main/java/com/fideljose/config/SwaggerConfig.java
+++ src/main/java/com/fideljose/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package com.fideljose.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.models.Contact;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,7 +21,8 @@ public class SwaggerConfig {
           .select()                                  
           .apis(RequestHandlerSelectors.basePackage("com.fideljose.controller"))  
           .paths(PathSelectors.any())                       
-          .build();                                           
+          .build()
+          .apiInfo(metaInfo());                                           
     }
 //	 @Bean
 //    public Docket api() { 
@@ -29,4 +32,18 @@ public class SwaggerConfig {
 //          .paths(PathSelectors.any())                       
 //          .build();                                           
 //    }
+
+	private ApiInfo metaInfo() {
+		@SuppressWarnings("deprecation")
+		ApiInfo apiInfo = new ApiInfo(
+			"title1",
+			"description2",
+			"version3",
+			"https://www.programcreek.com/java-api-examples/?api=springfox.documentation.builders.PathSelectors",
+			"Contacto: Fidel Jose Villero",
+			"license6",
+			"http://127.0.0.1:49811/browser/"
+		);
+		return apiInfo;
+	}
 }
